c6f01375d1ed26b1bdb3f07240183c1618181e00
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-12-15 18:02:30 +0600

    job application api make or jobs details and my application section a[i make complete

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const express = require('express');
-const app = express()
-const port = process.env.PORT || 5000
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldsdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,28 +16,48 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const jobCollection = client.db("job-portal").collection("jobs");
+    const jobApplication = client.db("job-portal").collection("application");
 
-    const jobCollection = client.db('job-portal').collection('jobs')
-
-    // create a new job api section
-    app.post('/jobs' , async(req , res)=>{
-        const data = req.body ;
-        const result = await jobCollection.insertOne(data)
-        res.send(result)
-    })
     // read all job api section
-    app.get('/jobs' , async(req , res)=>{
-        const cursor = jobCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // read jobs details section api
+    app.get("/job/:id" , async(req , res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const cursor = await jobCollection.find(query).toArray();
+      res.send(cursor)
     })
+    // my application view section api
+    app.get('/my-application/:email' , async(req , res)=>{
+      const email = req.params.email;
+      const cursor = {applicant_email: email}
+      const result = await jobApplication.find(cursor).toArray()
+      res.send(result)
+    })
+    // create a new job api section
+    app.post("/jobs", async (req, res) => {
+      const data = req.body;
+      const result = await jobCollection.insertOne(data);
+      res.send(result);
+    });
 
-
+    // create user application api
+    app.post("/job-application", async (req, res) => {
+      const data = req.body;
+      const result = await jobApplication.insertOne(data);
+      res.send(result);
+      console.log(data);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -55,12 +71,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/' , (req , res)=>{
-    res.send('server id running')
-})
-app.listen(port , ()=> {
-    console.log("server id running")
-})
+app.get("/", (req, res) => {
+  res.send("server id running");
+});
+app.listen(port, () => {
+  console.log("server id running");
+});
